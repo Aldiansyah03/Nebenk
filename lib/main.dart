cee5908f32d41c6dec8cbbfd438dd9eee08e5e94
Aldiansyah03 <135023769+Aldiansyah03@users.noreply.github.com>
2023-10-22 13:17:51 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:nebengk/Pages/LoginScreen.dart';
 import 'package:nebengk/Pages/SplashScreen.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
