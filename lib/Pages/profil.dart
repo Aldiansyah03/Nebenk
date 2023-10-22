0e48013e1e833b27d387c44b588820800f99e855
Roymundus <11211009@student.itk.ac.id>
2023-10-22 12:43:05 +0800

    memperbaiki profil

--- lib/Pages/profil.dart
+++ lib/Pages/profil.dart
@@ -1,93 +1,48 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profil Saya'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            width: 150.0,
-            height: 150.0,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('assets/profil.png'),
-              ),
-            ),
-          ),
-          SizedBox(height: 20.0),
-          Text(
-            'Aldiansyah',
-            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10.0),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.location_on),
-                  title: Text(
-                      'Alamat: Jl.Marsma R iswahyudi, Sungai Nangka rt 10'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.phone),
-                  title: Text('Nomor Telepon: 081649572780'),
-                ),
-                // Tambahkan info tambahan lainnya sesuai kebutuhan Anda
-              ],
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // Tindakan yang diambil saat tombol "Edit Profil" diklik
-            },
-            child: Text('Edit Profil'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ProfilePageScreen extends StatelessWidget {
-  const ProfilePageScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        CircleAvatar(
-          radius: 100,
-          child: Image.asset("assets/icons/profil.png"),
+        appBar: AppBar(
+          title: const Text('Profil'),
+          backgroundColor: const Color(0xFF3668B2),
         ),
-        const SizedBox(height: 20),
-        itemProfile('Nama', 'Muhammad Aldiansyah', CupertinoIcons.person),
-        const SizedBox(height: 10),
-        itemProfile('Nomor Telepon', '085266127946', CupertinoIcons.phone),
-        const SizedBox(height: 10),
-        itemProfile('Alamat', 'Sepinggan, Balikpapan', CupertinoIcons.location),
-        const SizedBox(height: 10),
-        itemProfile('Email', 'aldiansyah@gmail.com', CupertinoIcons.mail),
-        const SizedBox(height: 20),
-        SizedBox(
-          width: double.infinity,
-          child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(padding: EdgeInsets.all(15)),
-              child: const Text('Edit Profil')),
-        )
-      ],
-    );
+        body: ListView(
+          children: [
+            CircleAvatar(
+              radius: 100,
+              child: Image.asset("assets/icons/profil.png"),
+            ),
+            itemProfile('Nama', 'Muhammad Aldiansyah', CupertinoIcons.person),
+            const SizedBox(height: 10),
+            itemProfile('Nomor Telepon', '085266127946', CupertinoIcons.phone),
+            const SizedBox(height: 10),
+            itemProfile(
+                'Alamat', 'Sepinggan, Balikpapan', CupertinoIcons.location),
+            const SizedBox(height: 10),
+            itemProfile('Email', 'aldiansyah@gmail.com', CupertinoIcons.mail),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(padding: EdgeInsets.all(15)),
+                  child: const Text('Edit Profil')),
+            )
+          ],
+        ));
   }
 
-  Widget itemProfile(String title, String subtitle, IconData icon) {
+  itemProfile(String title, String subtitle, IconData icon) {
     return Container(
       decoration: BoxDecoration(
           color: Colors.white,
