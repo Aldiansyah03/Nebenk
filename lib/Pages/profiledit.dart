cffd360924284344d70372500b14b9385c712b21
Roymundus <11211009@student.itk.ac.id>
2023-11-06 11:21:24 +0800

    edit profile, masih belum bisa nampilin data awal

--- lib/Pages/profiledit.dart
+++ lib/Pages/profiledit.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: non_constant_identifier_names, sort_child_properties_last, deprecated_member_use
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class EditProfile extends StatefulWidget {
   @override
@@ -7,42 +11,112 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
-  bool isObscurePassword = true;
+  User? currentUser;
+  String? nama = '';
+  String? nomor_telepon = '';
+  String? alamat = '';
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  Future<void> getData() async {
+    currentUser = FirebaseAuth.instance.currentUser;
+
+    if (currentUser != null) {
+      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
+          // final userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser!.uid)
+          .get();
+
+      if (userSnapshot.exists) {
+        Map<String, dynamic> userData =
+            userSnapshot.data() as Map<String, dynamic>;
+        setState(() {
+          nama = userData['nama'];
+          nomor_telepon = userData['nomor_telepon'];
+          alamat = userData['alamat'];
+        });
+      }
+    }
+  }
+
+  Future<void> saveUserData() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      User? currentUser = FirebaseAuth.instance.currentUser;
+      if (currentUser != null) {
+        Map<String, dynamic> userData = {
+          'nama': nama,
+          'nomor_telepon': nomor_telepon,
+          'alamat': alamat,
+        };
+
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(currentUser.uid)
+            .update(userData)
+            .then((_) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Data Berhasil diubah!!')));
+        }).catchError((error) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Gagal mengubah data!!')));
+        });
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Gagal mengubah data!!')));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Edit Profil"),
-          backgroundColor: const Color(0xFF3668B2),
-        ),
-        body: Container(
-          padding: const EdgeInsets.only(left: 15, top: 20, right: 15),
-          child: GestureDetector(
-            onTap: () {
-              FocusScope.of(context).unfocus();
-            },
+      appBar: AppBar(
+        title: const Text("Edit Profil"),
+        backgroundColor: const Color(0xFF3668B2),
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(left: 15, top: 20, right: 15),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Form(
+            key: _formKey,
             child: ListView(
               children: [
                 Center(
                   child: Stack(
                     children: [
                       Container(
-                          width: 130,
-                          height: 130,
-                          decoration: BoxDecoration(
-                              border: Border.all(width: 4, color: Colors.white),
-                              boxShadow: [
-                                BoxShadow(
-                                    spreadRadius: 2,
-                                    blurRadius: 10,
-                                    color: Colors.black.withOpacity(0.1))
-                              ],
-                              shape: BoxShape.circle,
-                              image: const DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(
-                                    'https://cdn.pixabay.com/photo/2015/01/06/16/14/woman-590490_1280.jpg'),
-                              ))),
+                        width: 130,
+                        height: 130,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 4, color: Colors.white),
+                          boxShadow: [
+                            BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                            )
+                          ],
+                          shape: BoxShape.circle,
+                          image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              'https://cdn.pixabay.com/photo/2015/01/06/16/14/woman-590490_1280.jpg',
+                            ),
+                          ),
+                        ),
+                      ),
                       Positioned(
                         bottom: 0,
                         right: 0,
@@ -50,9 +124,10 @@ class _EditProfileState extends State<EditProfile> {
                           height: 40,
                           width: 40,
                           decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(width: 4, color: Colors.white),
-                              color: Colors.blue),
+                            shape: BoxShape.circle,
+                            border: Border.all(width: 4, color: Colors.white),
+                            color: Colors.blue,
+                          ),
                           child: const Icon(Icons.edit, color: Colors.white),
                         ),
                       )
@@ -60,41 +135,60 @@ class _EditProfileState extends State<EditProfile> {
                   ),
                 ),
                 const SizedBox(height: 30),
-                buildTextField("Nama Lengkap", "Muhammad Aldiansyah", false),
-                // buildTextField("E-mail", "aldiansyah@gmail.com", false),
-                buildTextField("Password", "aldi1234567", true),
-                buildTextField("No Handphone", "085266127946", false),
-                buildTextField("Alamat", "Sepinggan, Balikpapan", false),
+                _buildTextField("Nama Lengkap", nama, CupertinoIcons.person,
+                    (value) {
+                  nama = value;
+                }),
+                _buildTextField(
+                    "Nomor Telepon", nomor_telepon, CupertinoIcons.phone,
+                    (value) {
+                  nomor_telepon = value;
+                }),
+                _buildTextField("Alamat", alamat, CupertinoIcons.location,
+                    (value) {
+                  alamat = value;
+                }),
                 const SizedBox(height: 30),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     OutlinedButton(
-                      onPressed: () {},
-                      child: const Text("CANCEL",
-                          style: TextStyle(
-                              fontSize: 15,
-                              letterSpacing: 2,
-                              color: Colors.black)),
+                      onPressed: () {
+                        getData(); // Mengembalikan data awal
+                      },
+                      child: const Text(
+                        "BATAL",
+                        style: TextStyle(
+                          fontSize: 15,
+                          letterSpacing: 2,
+                          color: Colors.black,
+                        ),
+                      ),
                       style: OutlinedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(horizontal: 50),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
+                        padding: const EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
                     ),
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        saveUserData();
+                      },
                       child: const Text(
-                        "SAVE",
+                        "SIMPAN",
                         style: TextStyle(
-                            fontSize: 15,
-                            letterSpacing: 2,
-                            color: Colors.white),
+                          fontSize: 15,
+                          letterSpacing: 2,
+                          color: Colors.white,
+                        ),
                       ),
                       style: ElevatedButton.styleFrom(
                         primary: Colors.blue,
                         padding: const EdgeInsets.symmetric(horizontal: 50),
                         shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
                       ),
                     )
                   ],
@@ -102,31 +196,28 @@ class _EditProfileState extends State<EditProfile> {
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 
-  Widget buildTextField(
-      String labeltext, String placeholder, bool isPasswordTextField) {
+  Widget _buildTextField(
+    String labeltext,
+    String? value,
+    IconData icon,
+    void Function(String?)? onSaved,
+  ) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 30),
-      child: TextField(
-        obscureText: isPasswordTextField ? isObscurePassword : false,
+      child: TextFormField(
+        onSaved: onSaved,
+        initialValue: value,
         decoration: InputDecoration(
-            suffixIcon: isPasswordTextField
-                ? IconButton(
-                    icon: const Icon(Icons.remove_red_eye, color: Colors.grey),
-                    onPressed: () {
-                      setState(() {
-                        isObscurePassword = !isObscurePassword;
-                      });
-                    })
-                : null,
-            contentPadding: const EdgeInsets.only(bottom: 5),
-            labelText: labeltext,
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            hintText: placeholder,
-            hintStyle: const TextStyle(
-                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey)),
+          contentPadding: const EdgeInsets.only(bottom: 5),
+          labelText: labeltext,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          prefixIcon: Icon(icon),
+        ),
       ),
     );
   }
